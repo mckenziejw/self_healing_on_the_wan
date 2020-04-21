22d22ba892369ff6a2ac86040df8f3d9d57e3af7
Joshua McKenzie <jmckenzie@juniper.net>
2020-04-21 10:30:52 -0700

    Added Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3
+
+LABEL Josh McKenzie "jmckenzie@juniper.net"
+
+COPY ./requirements.txt /app/requirements.txt
+
+WORKDIR /app
+
+RUN pip install -r requirements.txt
+
+COPY . /app
+
+ENTRYPOINT [ "python" ]
+
+CMD [ "app.py" ]
--- app/healthbot_listener.py
+++ app/healthbot_listener.py
@@ -19,11 +19,11 @@ def app_message_post():
         abort(400, message="Expected Content-Type = application/json")
     try:
         data = request.json
-        print data
+        print(data)
         message = data['message']
-        print "message " + message
+        print("message " + message)
         playbook_name = data['keys']['_playbook_name']
-        print "playbook_name " + playbook_name
+        print("playbook_name " + playbook_name)
         #test_name = data['keys']['test-name']
         #print "interface-name " + test_name 
         device_id = data['device-id']
@@ -40,7 +40,7 @@ def app_message_post():
         #        print 'LSP path can be changed back'
         #return json.dumps({'result': 'OK'})
         if playbook_name == "cpu_openconfig":
-            print "received cpu high alert"
+            print("received cpu high alert")
             if "exceeds high threshold" in message:
                 print 'CPU HIGH UTIL DETECTED for ' + device_id
                 print 'PERFORMING EXHUASTIVE LINK FAILURE SIMULATION for ' + device_id
@@ -50,46 +50,46 @@ def app_message_post():
                 maintenance_event = user_functions.create_maintenance(rest_payload)
                 maintenance_index = maintenance_event.json()['maintenanceIndex']
                 check_simulation = user_functions.check_if_simulation_pass()
-                print "simulation result " + check_simulation
+                print("simulation result " + check_simulation)
                 user_functions.delete_maintenance(maintenance_index)
-                print "delete temp maintenace"
+                print("delete temp maintenace")
                 if check_simulation == 'true':
-                    print 'CPU HIGH UTIL DETECTED PUT NODE UNDER MAINTENANCE::'
+                    print('CPU HIGH UTIL DETECTED PUT NODE UNDER MAINTENANCE::')
                     # pprint.pprint(data)
                     #print "rest_node_name, rest_index_number" +  rest_node_name +  rest_index_number
                     rest_payload = user_functions.generate_maitenance_json(rest_index_number, 'for_maint', 'node')
-                    print rest_payload
+                    print(rest_payload)
                     user_functions.create_maintenance(rest_payload)
                 else:
-                    print 'CANNOT PUT ' + device_id + ' UNDER MAINTENANCE. EXHUASTIVE FAILURE SIMULATION NOT PASSED' 
+                    print('CANNOT PUT ' + device_id + ' UNDER MAINTENANCE. EXHUASTIVE FAILURE SIMULATION NOT PASSED')
             elif "is normal" in message:
                 #print 'DATA_INACTIVE :: ', pprint.pprint(data)
-                print 'CPU util back to normal. '
+                print('CPU util back to normal. ')
         print '###############################'
         if playbook_name == "probe_delay":
-            print "received delay alert"
+            print("received delay alert")
             source_address = data['keys']['source-address']
             #print "interface-ip " + source_address
             #target_address = data['keys']['target_address']
             #print "message" + message
             if "exceeds delay threshold" in message:
-                print "HIGH DELAY DETECTED for  " + device_id + " " + source_address 
-                print "PERFORMING EXHUASTIVE LINK FAILURE SIMULATION for " + device_id + " " + source_address
+                print("HIGH DELAY DETECTED for  " + device_id + " " + source_address )
+                print("PERFORMING EXHUASTIVE LINK FAILURE SIMULATION for " + device_id + " " + source_address)
                 #create maintenance for simulation purpose
                 rest_index_number = user_functions.get_link_info_from_ip(source_address)
                 rest_payload = user_functions.generate_maitenance_json(rest_index_number, 'for_simulation', 'link')
                 maintenance_event = user_functions.create_maintenance(rest_payload)
                 maintenance_index = maintenance_event.json()['maintenanceIndex']
                 check_simulation = user_functions.check_if_simulation_pass()
-                print "SIMULATION RESULT " + check_simulation
+                print("SIMULATION RESULT " + check_simulation)
                 user_functions.delete_maintenance(maintenance_index)
                 #print "delete temp maintenace"
                 if check_simulation == "true":
-                    print "HIGH DELAY DETECTED PUT LINK UNDER MAINTENANCE::"
+                    print("HIGH DELAY DETECTED PUT LINK UNDER MAINTENANCE::")
                     # pprint.pprint(data)
                     #print "rest_node_name, rest_index_number" +  rest_node_name +  rest_index_number
                     rest_payload = user_functions.generate_maitenance_json(rest_index_number, 'for_maint', 'link')
-                    print rest_payload
+                    print(rest_payload)
                     user_functions.create_maintenance(rest_payload)
                 else:
                     print "CANNOT PUT " + device_id + " " + source_address + " UNDER MAINTENANCE. EXHUASTIVE FAILURE SIMULATION NOT PASSED"
